cb6f721c93a3434353d3cafeeeee886d78855c3f
Cheyenne Hwang <cmhwang@email.wm.edu>
2022-11-18 20:11:03 -0500

    added to generating screen ui

--- src/main/java/edu/wm/cs/cs301/cheyennehwang/gui/LosingActivity.java
+++ src/main/java/edu/wm/cs/cs301/cheyennehwang/gui/LosingActivity.java
@@ -1,6 +1,19 @@
 package edu.wm.cs.cs301.cheyennehwang.gui;
 
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import edu.wm.cs.cs301.cheyennehwang.R;
+
 public class LosingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generationlayout);
+
+    }
 }
